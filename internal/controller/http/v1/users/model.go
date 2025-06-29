faafe12f9793751fe213aa83bef12c2fee80d720
Mihail Solovev <youoliver@mail.ru>
2025-06-29 21:12:54 +0300

    feat: added user profile card handler

--- internal/controller/http/v1/users/model.go
+++ internal/controller/http/v1/users/model.go
@@ -1,7 +1,27 @@
 package users
 
+import "time"
+
+// GenderEnum is type for gender enum.
+type GenderEnum int16 // @name GenderEnum
+
+// Gender enum.
+const (
+	MALE   GenderEnum = 1 // Male
+	FEMALE GenderEnum = 2 // Female
+)
+
 // UserInfoOutput is output model of user info request.
 type UserInfoOutput struct {
 	ID       int64  `json:"id"`        // User ID.
 	FullName string `json:"full_name"` // User full name.
 } // @name UserInfoOutput
+
+// UserProfileCardOutput is output model of user profile card request.
+type UserProfileCardOutput struct {
+	ID            int64       `json:"id"`              // User ID.
+	FullName      string      `json:"full_name"`       // User full name.
+	Gender        *GenderEnum `json:"gender"`          // User gender.
+	DateOfBirth   *time.Time  `json:"date_of_birth"`   // User date of birth.
+	AvatarFileKey *string     `json:"avatar_file_key"` // User avatar file key.
+} // @name UserProfileCardOutput
--- internal/controller/http/v1/users/users.go
+++ internal/controller/http/v1/users/users.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	lsuserspb "github.com/p1xray/love-signal-protos/gen/go/users"
 	"love-signal-api/internal/server"
+	"time"
 )
 
 // Routes provides routes for users.
@@ -18,6 +19,7 @@ func InitRoutes(api *gin.RouterGroup, grpcUsersClient lsuserspb.UsersClient) {
 	profile := api.Group("/users")
 	{
 		profile.GET("/user-info", r.userInfo)
+		profile.GET("/profile", r.userProfileCard)
 	}
 }
 
@@ -50,4 +52,56 @@ func (r *Routes) userInfo(c *gin.Context) {
 	}
 
 	server.SuccessResponse(c, &userInfo)
+}
+
+// User profile card.
+//
+//	@Summary		User profile card
+//	@Description	User profile card
+//	@Tags			Users
+//	@Id 			userProfileCard
+//	@Produce		json
+//	@Security 		ApiKeyAuth
+//	@Success		200	{object}  server.dataResponse[UserProfileCardOutput]
+//	@Failure		500	{object}  server.dataResponse[UserProfileCardOutput]
+//	@Router			/api/v1/users/profile [get]
+func (r *Routes) userProfileCard(c *gin.Context) {
+	userExternalID := int64(1) // TODO: get it from token
+	
+	grpcUserDataRequest := &lsuserspb.GetUserDataByExternalIdRequest{UserExternalId: userExternalID}
+	grpcUserDataResponse, err := r.grpcUsersClient.GetUserDataByExternalId(c.Request.Context(), grpcUserDataRequest)
+	if err != nil {
+		// TODO: check error from gRPC server and return correct error
+
+		server.ErrorResponse[UserProfileCardOutput](c, err.Error())
+		return
+	}
+
+	var dateOfBirth *time.Time
+	if grpcUserDataResponse.GetDateOfBirth() != nil {
+		dateOfBirthValue := grpcUserDataResponse.GetDateOfBirth().AsTime()
+		dateOfBirth = &dateOfBirthValue
+	}
+
+	var gender *GenderEnum
+	if grpcUserDataResponse.GetGender() != lsuserspb.Gender_GENDER_UNSPECIFIED {
+		genderValue := GenderEnum(grpcUserDataResponse.GetGender())
+		gender = &genderValue
+	}
+
+	var avatarFileKey *string
+	if grpcUserDataResponse.GetAvatarFileKey() != nil {
+		avatarFileKeyValue := grpcUserDataResponse.GetAvatarFileKey().GetValue()
+		avatarFileKey = &avatarFileKeyValue
+	}
+
+	userProfileCard := UserProfileCardOutput{
+		ID:            grpcUserDataResponse.GetId(),
+		FullName:      grpcUserDataResponse.GetFullName(),
+		Gender:        gender,
+		DateOfBirth:   dateOfBirth,
+		AvatarFileKey: avatarFileKey,
+	}
+
+	server.SuccessResponse(c, &userProfileCard)
 }
